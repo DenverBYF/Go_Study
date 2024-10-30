cb3e33ac58485fd448a0aec282e1ce85cebb18c5
白一凡 <yifan.bai@mihoyo.com>
2024-10-30 19:50:27 +0800

    feat: leet

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"example.com/m/v2/structture"
-	"fmt"
 	"strconv"
 )
 
@@ -187,6 +186,21 @@ func validStrings(n int) []string {
 	return res
 }
 
+/*
+给你一个仅由数字组成的字符串 s，在最多交换一次 相邻 且具有相同 奇偶性 的数字后，返回可以得到的
+字典序最小的字符串。
+如果两个数字都是奇数或都是偶数，则它们具有相同的奇偶性。例如，5 和 9、2 和 4 奇偶性相同，而 6 和 9 奇偶性不同。
+*/
+func getSmallestString(s string) string {
+	for i := 0; i < len(s)-1; i++ {
+		iNow, _ := strconv.Atoi(string(s[i]))
+		iNext, _ := strconv.Atoi(string(s[i+1]))
+		if iNow > iNext && (iNow+iNext)%2 == 0 {
+			return s[:i] + string(s[i+1]) + string(s[i]) + s[i+2:]
+		}
+	}
+	return s
+}
+
 func main() {
-	fmt.Println([]int{1, 2, 3}[1:2])
 }
