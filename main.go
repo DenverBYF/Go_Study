4f5077c13439fb4cf0a204c44cadf5ce379f1168
白一凡 <yifan.bai@mihoyo.com>
2024-11-05 16:26:51 +0800

    leet

--- main.go
+++ main.go
@@ -374,6 +374,26 @@ func judgeSquareSum(c int) bool {
 	return false
 }
 
+/*
+给你两个 正 整数 x 和 y ，分别表示价值为 75 和 10 的硬币的数目。
+Alice 和 Bob 正在玩一个游戏。每一轮中，Alice 先进行操作，Bob 后操作。每次操作中，玩家需要拿出价值 总和 为 115 的硬币。如果一名玩家无法执行此操作，那么这名玩家 输掉 游戏。
+两名玩家都采取 最优 策略，请你返回游戏的赢家。
+*/
+func losingPlayer(x int, y int) string {
+	i := 0
+	for ; ; i++ {
+		if x <= 0 || y < 4 {
+			break
+		}
+		x -= 1
+		y -= 4
+	}
+	if i%2 == 0 {
+		return "Bob"
+	}
+	return "Alice"
+}
+
 func main() {
 	fmt.Println(maxEnergyBoost([]int{1, 3, 1}, []int{3, 1, 1}))
 }
